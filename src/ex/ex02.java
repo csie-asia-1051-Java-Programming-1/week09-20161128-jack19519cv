5d7c616ae51f37caf46e298d8d10e27e99f1c8d5
jack19519cv <105021059@live.asia.edu.tw>
2017-01-01 22:27:39 +0800

    save

--- src/ex/ex02.java
+++ src/ex/ex02.java
@@ -4,13 +4,35 @@
  並於主程式讓使用者輸入位學生的資料並根據平均成績由高至低輸出如下:
  ID    NAME     Chinese     English       Math        Avg
  * Date: 2016/11/28
- * Author: 1050210XX 周永振老師
+ * Author: 105021059 王俊鑌
  */
 
+import java.util.ArrayList;
+import java.util.Scanner;
 public class ex02 {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		ArrayList <Student> studList = new ArrayList <Student>();
+		studList.add(new Student(scn.next(),scn.next()));
+		studList.get(1).show();
+
+	}
+	public class Student {
+		
+		private String id , name;
+		private ArrayList <Course> courList = new ArrayList<Course>();
+		public Student(String name1 , String id1){
+			name = name1;
+			id = id1;
+		}
+		public void show(){
+			System.out.println("name:" + name);
+			System.out.println("id:" + id);
+		}
+
+	}
+	public class Course {
 
 	}
 
--- src/hw/hw01.java
+++ src/hw/hw01.java
@@ -2,24 +2,49 @@ package hw;
 
 import java.util.Scanner;
 
+
 /*
  * Topic: 讓使用者輸入一正整數，利用函式呼叫找出一連續整數數列讓該數列的和為此一正整數，有些正整數並不能找出連續整數，若找不到時，請印出「No」。
+
  例如: 15 =1+2+3+4+5 =4+5+6=7+8
  * Date: 2016/11/28
- * Author: 1050210XX 周永振老師
+ * Author: 105021059 王俊鑌
  */
+import java.util.Scanner;
 public class hw01 {
 
 	public static void main(String[] args) {
-		Scanner scn =new Scanner(System.in);
-        int n=scn.nextInt();
-        for(int i=0;i<n;i++){
-        	System.out.print(n+"=");
-        	System.out.print(i);
-        	
-        	
-        }
-
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		int sum = 0; 
+		int a;
+        boolean cie = false; 
+        for(int j = 1 ; j < (n/2)+1 ; j++){
+            for (int i = j; i <= n; i++) {  
+                sum += i;  
+                if (n == sum) {  
+                    cie = true; 
+                    for (int k = j; k <= i; k++) {  
+                        System.out.print(k );  //
+                        
+                        for (int c = j; c <= k; c++) {  //i=
+                            System.out.print("+"); 
+                            
+                        } 
+                        }  
+                    
+                    
+                    
+                    System.out.println("=");
+                }  
+            }  
+            sum = 0;  //歸零
+        }  
+        if (cie == false) {  
+            System.out.println("No");  
+        }  
 	}
-
-}
+		
+	}
+	
